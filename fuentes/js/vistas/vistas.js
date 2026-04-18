658f93810124f384450886f939d44f4257f7a408
Vicente casa <vicenteayusopi@gmail.com>
2026-04-18 16:08:37 +0200

    arreglo del codigo

--- fuentes/js/vistas/vistas.js
+++ fuentes/js/vistas/vistas.js
@@ -1,10 +1,6 @@
 const botonFormulario = document.querySelector('#botonFormulario');
 const botonListado = document.querySelector('#botonListado');
 
-const botonGuardar = document.querySelector('#guardarPersonaje');
-const botonBorrar = document.querySelector('#borrarPersonajes');
-const botonModificar = document.querySelector('#modificarPersonajes');
-
 const vistaInicio = document.querySelector('#cuerpoMenu');
 const vistaFormulario = document.querySelector('#vistaFormulario');
 const vistaListado = document.querySelector('#vistaListado');
@@ -13,28 +9,10 @@ function mostrarVista(seccion) {
     vistaInicio.style.display = 'none';
     vistaFormulario.style.display = 'none';
     vistaListado.style.display = 'none';
-
     seccion.style.display = 'block';
 }
 
-//NAVEGAR
 botonFormulario.onclick = () => mostrarVista(vistaFormulario);
 botonListado.onclick = () => mostrarVista(vistaListado);
-
-// Accion
-botonGuardar.onclick = () => {
-    alert("Guardando.");
-    mostrarVista(vistaListado); 
-};
-
-botonModificar.onclick = () => {
-    alert("Modificando datos...");
-    mostrarVista(vistaFormulario);
-};
-//BORRAR
-botonBorrar.onclick = () => {
-    alert("Personaje eliminado");
-    mostrarVista(vistaListado);
-};
 
 mostrarVista(vistaInicio);
