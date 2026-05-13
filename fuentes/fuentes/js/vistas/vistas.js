e4e2e6263861b45aa65974754fe2b061bd99ca18
Rodrigo Prieto <rprietop04@educarex.es>
2026-05-13 11:57:44 +0200

    Arreglo programa

---
+++ fuentes/fuentes/js/vistas/vistas.js
@@ -0,0 +1,32 @@
+const botonInicio = document.querySelector('#botonInicio');
+const botonFormulario = document.querySelector('#botonFormulario');
+const botonListado = document.querySelector('#botonListado');
+const botonJugar = document.querySelector('#botonJugar');
+const botonNoticias = document.querySelector('#botonNoticias');
+const botonContacto = document.querySelector('#botonContacto');
+
+const vistaInicio = document.querySelector('#cuerpoMenu');
+const vistaFormulario = document.querySelector('#vistaFormulario');
+const vistaListado = document.querySelector('#vistaListado');
+const vistaNoticias = document.querySelector('#vistaNoticias');
+const vistaContacto = document.querySelector('#vistaContacto');
+const vistaJugar = document.querySelector('#vistaJugar');
+
+function mostrarVista(seccion) {
+    vistaInicio.style.display = 'none';
+    vistaFormulario.style.display = 'none';
+    vistaListado.style.display = 'none';
+    vistaNoticias.style.display = 'none';
+    vistaContacto.style.display = 'none';
+    vistaJugar.style.display = 'none';
+    seccion.style.display = 'block';
+}
+
+botonInicio.onclick = () => mostrarVista(cuerpoMenu);
+botonFormulario.onclick = () => mostrarVista(vistaFormulario);
+botonListado.onclick = () => mostrarVista(vistaListado);
+botonJugar.onclick = () => mostrarVista(vistaJugar);
+botonNoticias.onclick = () => mostrarVista(vistaNoticias);
+botonContacto.onclick = () => mostrarVista(vistaContacto);
+
+mostrarVista(vistaInicio);
